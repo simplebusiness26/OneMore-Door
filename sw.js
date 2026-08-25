1f69101bde2adb887eb7285ff6bfca09c037300c
simplebusiness26 <simplebussiness26@gmail.com>
2026-08-25 02:55:57 +0100

    Cache objective mission system offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='one-more-door-v2-1';
-const ASSETS=['./','./index.html','./styles.css','./v2.css','./core.js','./flow.js','./mechanics.js','./progression.js','./render.js','./v2.js','./v2-fixes.js','./boot.js','./manifest.webmanifest','./icons/icon.svg'];
+const CACHE='one-more-door-v3';
+const ASSETS=['./','./index.html','./styles.css','./v2.css','./missions.css','./core.js','./flow.js','./mechanics.js','./progression.js','./render.js','./v2.js','./v2-fixes.js','./missions.js','./boot.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match('./index.html'))));});
